904e73fe1f120b0332dd53afd3b29677edb05955
Ryota <ryoenoki2013@gmail.com>
2022-01-26 01:25:40 +0900

    added card component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Card from "./components/Card"
 import './style.css'
 
 function App() {
@@ -8,6 +9,14 @@ function App() {
     <div className="App">
       < Navbar />
       <Hero />
+      <Card
+      img="katie-zaferes.png"
+      rating={5.0}
+      review={6}
+      country="Japan"
+      title="Life Lessons with Katie Zaferes"
+      price={136}
+      />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from "react";
+import star from "../images/star.png"
+import kate from "../images/katie-zaferes.png"
+
+export default function Card(props) {
+  return(
+    <div className="card">
+      <img src={kate} className="card--image" alt={props.title} />
+      <div className="card--stats">
+        <img src={star} className="card--star" alt="star"/>
+        <span>{props.rating}</span>
+        <span className="gray">({props.review}) •</span>
+        <span className="gray">{props.country}</span>
+      </div>
+      <p>{props.title}</p>
+      <p><span className="bold">From {props.price}</span> / person</p>
+    </div>
+  )
+}
