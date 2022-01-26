142545728d006b91f1139bc4f0f0c1086bfc7c81
Ryota <ryoenoki2013@gmail.com>
2022-01-27 00:56:15 +0900

    added data.js and missing images

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import data from './data'
 import './style.css'
 
 function App() {
+  const card = data.map(item => {
+    return(
+      <Card
+      key={item.id}
+      img={item.coverImg}
+      status="SOLD OUT"
+      rating={item.stats.rating}
+      review={item.stats.reviewCount}
+      country={item.location}
+      title={item.title}
+      price={item.price}
+      />
+    )
+  })
   return (
     <div className="App">
       < Navbar />
-      {/* <Hero /> */}
-      <Card
-      img="katie-zaferes.png"
-      status="SOLD OUT"
-      rating={5.0}
-      review={6}
-      country="USA"
-      title="Life Lessons with Katie Zaferes"
-      price={136}
-      />
+      <Hero />
+      {card}
     </div>
   );
 }
