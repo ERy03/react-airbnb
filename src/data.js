a7eed7064f4f5e6cfff24f8f7408c7cb195ed387
Ryota <ryoenoki2013@gmail.com>
2022-01-27 01:47:36 +0900

    refactoring

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 export default [
-  {
+    {
     id: 1,
     title: "Life Lessons with Katie Zaferes",
     description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
@@ -11,8 +11,8 @@ export default [
     },
     location: "Online",
     openSpots: 0,
-  },
-  {
+},
+{
     id: 2,
     title: "Learn Wedding Photography",
     description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
